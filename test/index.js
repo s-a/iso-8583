003314b19eba9e3432cfe18522c633b599d5de5e
s-a <stephan.ahlf@gmail.com>
2016-06-03 17:10:18 +0200

    add human readable tests

--- test/index.js
+++ test/index.js
@@ -2,63 +2,69 @@ var ISO8583 = require('../');
 var assert = require('assert');
 
 
-describe('native extension', function() {
-  it('should export a wrapped object', function() {
-    var message = new ISO8583.Message(0);
+	describe('native extension', function() {
+		it('should export a wrapped object', function() {
+			var message = new ISO8583.Message();
 
-    var messageFields = [
-      [0, "1234"],
-      [2, "1234567890123456"],
-      [4, "5699"],
-      [11, "234"],
-      [39, "4"],
-      [41, "12345"],
-      [42, "678901234"],
-      [125, "BLAH BLAH"]
-    ];
+			assert.equal(typeof(message.pack), "function");
+			assert.equal(typeof(message.unpack), "function");
+		});
+	});
 
-    
-    var packedMessage = message.pack(messageFields);
-    console.log("packedMessage", packedMessage);
-    assert.equal(packedMessage.length, 72);
-    
-  });
+	describe('Pack ISO8583 message', function() {
+		it('should pack message values and return an Array of HEX values', function() {
 
-  /*
-  it('should export function that returns nothing', function() {
-    assert.equal(ISO8583.nothing(), undefined);
-  });
+			var messageFields = [
+				[0, "1234"],
+				[2, "1234567890123456"],
+				[4, "5699"],
+				[11, "234"],
+				[39, "4"],
+				[41, "12345"],
+				[42, "678901234"],
+				[125, "BLAH BLAH"]
+			];
 
-  it('should export a function that returns a string', function() {
-    assert.equal(typeof ISO8583.aString(), 'string');
-  });
+			var message = new ISO8583.Message();
+			var packedMessage = message.pack(messageFields).join(" ");
+			assert.equal(packedMessage, "12 34 D0 20 00 00 02 C0 00 00 00 00 00 00 00 00 00 08 16 12 34 56 78 90 12 34 56 00 00 00 00 56 99 00 02 34 00 04 31 32 33 34 35 20 20 20 36 37 38 39 30 31 32 33 34 20 20 20 20 20 20 00 09 42 4C 41 48 20 42 4C 41 48");
+		})
+	});
 
-  it('should export a function that returns a boolean', function() {
-    assert.equal(typeof ISO8583.aBoolean(), 'boolean');
-  });
+	/*
+	it('should export function that returns nothing', function() {
+		assert.equal(ISO8583.nothing(), undefined);
+	});
 
-  it('should export function that returns a number', function() {
-    assert.equal(typeof ISO8583.aNumber(), 'number');
-  });
+	it('should export a function that returns a string', function() {
+		assert.equal(typeof ISO8583.aString(), 'string');
+	});
 
-  it('should export function that returns an object', function() {
-    assert.equal(typeof ISO8583.anObject(), 'object');
-  });
+	it('should export a function that returns a boolean', function() {
+		assert.equal(typeof ISO8583.aBoolean(), 'boolean');
+	});
 
-  it('should export function that returns an object with a key, value pair', function() {
-    assert.deepEqual(ISO8583.anObject(), {'key': 'value'});
-  });
+	it('should export function that returns a number', function() {
+		assert.equal(typeof ISO8583.aNumber(), 'number');
+	});
 
-  it('should export function that returns an array', function() {
-    assert.equal(Array.isArray(ISO8583.anArray()), true);
-  });
+	it('should export function that returns an object', function() {
+		assert.equal(typeof ISO8583.anObject(), 'object');
+	});
 
-  it('should export function that returns an array with some values', function() {
-    assert.deepEqual(ISO8583.anArray(), [1, 2, 3]);
-  });
+	it('should export function that returns an object with a key, value pair', function() {
+		assert.deepEqual(ISO8583.anObject(), {'key': 'value'});
+	});
 
-  it('should export function that calls a callback', function(done) {
-    ISO8583.callback(done);
-  });
-  */
-});
+	it('should export function that returns an array', function() {
+		assert.equal(Array.isArray(ISO8583.anArray()), true);
+	});
+
+	it('should export function that returns an array with some values', function() {
+		assert.deepEqual(ISO8583.anArray(), [1, 2, 3]);
+	});
+
+	it('should export function that calls a callback', function(done) {
+		ISO8583.callback(done);
+	});
+	*/
